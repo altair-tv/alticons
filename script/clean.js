85cf141565c26490c32c94edff5884c9cd6f64ce
Bryan Veloso <bryan@avalonstar.com>
2021-01-20 12:18:23 -0800

    Refactor, simplify, focus on Chakra usage.

--- script/clean.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-try {
-  const rimraf = require('rimraf')
-
-  rimraf.sync(`${__dirname}/../packages/*/dist`)
-} catch (e) {
-  console.error("Failed to clean up, perhaps rimraf isn't installed?")
-  console.error(e)
-}
